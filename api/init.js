1ff3a9a93fa36472b79d9a01c2757d820fb27e2c
Yago <yagop@users.noreply.github.com>
2020-02-25 17:31:53 +0900

    Use mongoose

---
+++ api/init.js
@@ -0,0 +1,18 @@
+const { User } = require('../lib/db');
+
+module.exports = async (req, res) => {
+    const found = await User.findOne({ name: 'Rodrigo' });
+
+    if (!found) {
+        const rodrigo = new User({
+            name: 'Rodrigo',
+            life: 100,
+            toxicity: 0,
+            potions: 0,
+        });
+        const resp = await rodrigo.save();
+        res.status(201).send('User Rodrigo created. %j', resp);
+    } else {
+        res.status(201).send('User rodrigo already exists. %j', found);
+    }
+};
